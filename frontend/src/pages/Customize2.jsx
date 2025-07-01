28798d132cd453b2eceb08f3b1805114e466e5cc
Om Sawarkar <132092944+Oms2004@users.noreply.github.com>
2025-07-01 14:23:18 +0530

    Update Customize2.jsx

--- frontend/src/pages/Customize2.jsx
+++ frontend/src/pages/Customize2.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { UserDataContext } from '../context/userContext';
+import { UserDataContext } from '../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import { IoArrowBack } from "react-icons/io5";
 import axios from 'axios';
